614e456860d62bcb9d404a658e2ad6b0a0153599
CrixxMadine <50016024+CrixxMadine@users.noreply.github.com>
2019-06-18 12:09:26 +0200

    BackUp, da ich die Tree-Dateien Mergen will
    Nur für BackUp-Zwecke

---
+++ AlgoDatPraktikum_neu/AlgoDatPraktikum/AlgoDatPraktikum/AVLTree.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoDatPraktikum
+{
+    abstract class Tree
+    {
+
+        protected bool elementOperationNotNecessary = false;  // flag for AVL and Treap, intended for special case in Insert- and Delete-function   ---   mfg Madini
+
+        protected class Element
+        {
+            public int wert;
+            public Element right;
+            public Element left;
+
+            public Element(int wert) => this.wert = wert;
+        }
+
+        public Tree() => wurzel = null;
+
+        protected Element wurzel;
+
+        public void Print()
+        {
+            Print(wurzel, "");
+        }
+
+        void Print(Element elem, string s)
+        {
+            if (elem != null)
+            {
+                Print(elem.right, s + "   ");
+                Console.WriteLine(s + elem.wert);
+                Print(elem.left, s + "   ");
+            }
+        }
+
+
+        // hinzugefügt von Madini für AVL
+        protected Element RotateLeft(Element parent)
+        {
+            Element child = parent.left;
+            parent.left = child.right;
+            child.right = parent;
+            return child;
+        }
+
+        protected Element RotateRight(Element parent)
+        {
+            Element child = parent.right;
+            parent.right = child.left;
+            child.left = parent;
+            return child;
+        }
+
+        protected Element RotateLeftRight(Element parent)
+        {
+            Element child = parent.left;
+            parent.left = RotateRight(child);
+            return RotateLeft(parent);
+        }
+
+        protected Element RotateRightLeft(Element parent)
+        {
+            Element child = parent.right;
+            parent.right = RotateLeft(child);
+            return RotateRight(parent);
+        }
+
+        // Ende hinzugefügt von Madini
+    }
+
+
+    // Ab hier Madini
+
+    class AVLTree : Tree, ISetSorted
+    {
+        public AVLTree() { }
+
+
+        // liefert die Höhe des (Teil-)Baums rekursiv als Integer
+        //  ----   Bestimmung durch Rekursion ist sehr rechenintensiv, bessere Idee:  Höhe als Variable in der Node speichern
+        //  ----   --->  noch zu erledigen, mfg Madini
+
+        int GetHeight(Element node)
+        {
+            int treeHeight = 0;
+            
+            if (node != null)
+            {
+                int leftSide = GetHeight(node.left);
+                int rightSide = GetHeight(node.right);
+
+                treeHeight = ((leftSide > rightSide) ? leftSide : rightSide) + 1;
+            }
+
+            return treeHeight;
+        }
+
+        // Liefert Hilfsgröße BalanceFaktor, zur Einschätzung wie ausgeglichen Baum ist
+        // BFaktor=0 : Ausgeglichen, B>1 : links überwiegt, B<1, rechts überwiegt
+        int BalanceFactor(Element node)
+        {
+            int leftSide = GetHeight(node.left);
+            int rightSide = GetHeight(node.right);
+            return leftSide - rightSide;
+        }
+
+        // Gleicht den Baum durch Rotation aus
+        Element BalanceTree(Element node)
+        {
+            int bFactor = BalanceFactor(node);
+
+            if (bFactor > 1)
+            {
+                if (BalanceFactor(node.left) > 0)
+                    node = RotateLeft(node);
+                else
+                    node = RotateLeftRight(node);
+            }
+
+            else if (bFactor < -1)
+            {
+                if (BalanceFactor(node.right) > 0)
+                    node = RotateRightLeft(node);
+                else
+                    node = RotateRight(node);
+            }
+
+            return node;
+        }
+
+        public bool Search(int key)
+        {
+            if (RecursiveSearch(key, wurzel) == null)
+                return false;
+
+            return true;
+        }
+
+        Element RecursiveSearch(int key, Element node)
+        {
+            if (node == null)
+                return node;
+
+            else if (key == node.wert)
+                return node;
+
+            else if (key < node.wert)
+                return RecursiveSearch(key, node.left);
+
+            else
+
+                return RecursiveSearch(key, node.right);              
+        }
+
+        public bool Insert(int elem)
+        {
+            Element newNode = new Element(elem);
+
+            if (wurzel == null)
+            {
+                wurzel = newNode;
+                return true;
+            }
+
+            Element tempWurzel = wurzel;
+
+            tempWurzel = RecursiveInsert(tempWurzel, newNode);
+
+
+            if(elementOperationNotNecessary)  // Element ist schon im Baum
+                return false;
+
+
+            wurzel = tempWurzel;
+            return true;
+        }
+
+
+        Element RecursiveInsert(Element node, Element newNode)
+        {
+            if (node == null)  // Blatt an das das Element hin müsste
+            {
+                node = newNode;
+                return node;
+            }
+
+            else if (node.wert == newNode.wert)  // Element ist schon in Liste
+            {
+                elementOperationNotNecessary = true;
+                return node;
+            }
+
+
+            else if (newNode.wert < node.wert)   // linker Teilbaum
+            {
+                node.left = RecursiveInsert(node.left, newNode);
+                node = BalanceTree(node);
+            }
+
+            else if (newNode.wert > node.wert)   // rechter Teilbaum
+            {
+                node.right = RecursiveInsert(node.right, newNode);
+                node = BalanceTree(node);
+            }
+
+            return node;
+        }
+
+        public bool Delete(int elem)
+        {
+
+            // -----------------  Baum wird unnötog zweimal durchlaufen - hier bessere Lösung finden wie bei Insert
+            if (!Search(elem))
+                return false;
+            // -----------------
+
+
+            if (wurzel == null)
+                return false;
+
+            wurzel = RecursiveDelete(wurzel, elem);
+
+            return true;
+        }
+
+
+        Element RecursiveDelete(Element node, int elem)
+        {
+            Element predecessor;
+
+            if (node == null)  
+            {
+                return null;
+            }
+
+            if (elem < node.wert)  // Suche linker Teilbaum
+            {
+                node.left = RecursiveDelete(node.left, elem);
+
+                if (BalanceFactor(node) == -2)
+                {
+                    if (BalanceFactor(node.right) <= 0)
+                        node = RotateRight(node);
+                    else
+                        node = RotateRightLeft(node);
+                }
+            }
+
+            else if (elem > node.wert)  // Suche rechter Teilbaum
+            {
+                //if (node.rechts == null)
+                //    return node; 
+
+                node.right = RecursiveDelete(node.right, elem);
+
+                if (BalanceFactor(node) == 2)
+                {
+                    if (BalanceFactor(node.left) >= 0)
+                        node = RotateLeft(node);
+
+                    else
+                        node = RotateLeftRight(node);
+
+                }
+            }
+
+            else  // das gesuchte Element wurde gefunden (node.wert == elem)
+            {
+                if (node.right != null)
+                {
+                    predecessor = node.right;  // eigentlicher Löschvorgang
+
+                    while (predecessor.left != null)
+                        predecessor = predecessor.left;
+
+
+                    node.wert = predecessor.wert;
+                    node.right = RecursiveDelete(node.right, predecessor.wert);
+
+                    if (BalanceFactor(node) == 2)
+                    {
+                        if (BalanceFactor(node.left) >= 0)
+                            node = RotateLeft(node);
+
+                        else
+                            node = RotateLeftRight(node);
+                    }
+
+                }
+
+                else  // Fall, node.links != null
+                {
+                    return node.left;
+                }
+        
+            }
+
+            return node;
+        }
+    }
+}
---
+++ AlgoDatPraktikum_neu/AlgoDatPraktikum/AlgoDatPraktikum/Arrays.cs
@@ -0,0 +1,188 @@
+﻿using System;
+
+namespace AlgoDatPraktikum
+{
+    /// <summary>
+    /// abstrakte Oberklasse Array 
+    /// </summary>
+    abstract class Array
+    {
+        public Array()
+        {
+            array = new int[100];
+            anz = 0;
+        }
+
+        protected int[] array;
+        protected int anz;
+        /// <summary>
+        /// Print Funktion ist für alle Arrayarten gleich
+        /// </summary>
+        public void Print()
+        {
+            foreach (int i in array)
+                if (i != 0)
+                    Console.WriteLine(i);
+        }
+
+        /// <summary>
+        /// search die auf indivuellen search mit spezifizierteren rückgabenwerten zurückgreift
+        /// </summary>
+        /// <param name="elem"></param>
+        /// <returns></returns>
+        public bool Search(int elem)
+        {
+            int index = Search(elem,0);
+            return array[index]==elem;
+        }
+
+
+        protected int Search(int elem,int index)
+        {
+            if (index == array.Length)
+                return -1;
+            if (array[index] == elem || array[index] == 0)
+                return index;
+            else
+                return Search(elem, index + 1);
+        }
+    }
+
+    /// <summary>
+    /// Mehrfache gleiche Elemente erlaubt und unsortiert
+    /// </summary>
+    class MultiSetUnsortedArray : Array, IMultiSet
+    {
+        public MultiSetUnsortedArray() : base() { }
+
+        public bool Insert(int elem)
+        {
+            if (anz == array.Length)
+                return false;
+            array[anz] = elem;
+            anz++;
+            return true;
+        }
+        public bool Delete(int elem)
+        {
+            int pos = Search(elem,0);
+            if (pos < 0 || array[pos] != elem)
+                return false;
+            else
+            {
+                array[pos] = array[--anz];
+                array[anz] = 0;
+                return true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Ein gleiches Element erlaubt aber unsortiert
+    /// </summary>
+    class SetUnsortedArray : MultiSetUnsortedArray, ISet
+    {
+
+        public SetUnsortedArray() : base() { }
+
+        public new bool Insert(int elem)
+        {
+            int index = Search(elem, 0);
+            if (index < 0 || array[index] == elem)
+                return false;
+            array[index] = elem;
+            anz++;
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Mehrere gleiche Elemente erlaubt aber sortiert
+    /// </summary>
+    class MultiSetSortedArray : Array, IMultiSetSorted
+    {
+        public MultiSetSortedArray() : base() { }
+
+
+        new protected int Search(int elem, int index)
+        {
+            if (array[0] == 0)
+                return 0;
+            if (anz == array.Length)
+                return -1;
+
+            int left = 0;
+            int right = anz;
+            int middle = 0;
+
+            while (left <= right)
+            {
+                middle = (left + right) / 2;
+                if (array[middle] == elem)
+                {
+                    return middle;
+                }
+                if (array[middle] < elem)
+                    left = middle + 1;
+                if (array[middle] > elem)
+                    right = middle - 1;
+            }
+            if (array[middle] != 0 && array[middle] < elem)
+                return middle + 1;
+            return middle;
+        }
+
+        public bool Insert(int elem)
+        {
+            if (anz == array.Length)
+                return false;
+            int pos = Search(elem, 0);
+            Insert(elem, pos);
+            return true;
+        }
+
+        protected void Insert(int elem, int pos)
+        {
+            for (int i = anz; i >= pos; i--)
+                array[i + 1] = array[i];
+            array[pos] = elem;
+            anz++;
+            return;
+        }
+
+        public bool Delete(int elem)
+        {
+            int pos = Search(elem, 0);
+            if (array[pos]!=elem)
+            {
+                return false;
+            }
+            for (int i = pos; i < anz; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            array[anz] = 0;
+            anz--;
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// Ein gleiches Element erlaubt und sortiert
+    /// </summary>
+    class SetSortedArray : MultiSetSortedArray, ISetSorted
+    {
+        public SetSortedArray() : base() { }
+
+        new public bool Insert(int elem)
+        {
+            if (anz == array.Length)
+                return false;
+            int pos = Search(elem,0);
+            if (array[pos]==elem)
+                return false;
+            Insert(elem, pos);
+            return true;
+        }
+    }
+}
